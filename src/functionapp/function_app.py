bda3c9464ea0a07e7805189ce4f0f35f4878ae01
guythetechie <31325620+guythetechie@users.noreply.github.com>
2024-10-31 17:00:22 +0000

    Updates

--- src/functionapp/function_app.py
+++ src/functionapp/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
-from functionapp.import_with_storage_queue import blueprint as service_bus_blueprint
+from import_with_storage_queue import blueprint as storage_queue_blueprint
 from import_on_timer import blueprint as timer_blueprint
 
 app = func.App()
-app.register_functions(service_bus_blueprint)
+app.register_functions(storage_queue_blueprint)
 app.register_functions(timer_blueprint)
