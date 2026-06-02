fd69a2e7c85101512cba2286c9fbe55eabe2d7dd
guythetechie <31325620+guythetechie@users.noreply.github.com>
2026-06-02 17:11:53 +0000

    Updates

--- src/functionapp/proxy_http_trigger.py
+++ src/functionapp/proxy_http_trigger.py
@@ -1,20 +1,12 @@
 import logging
 import azure.functions as func
-import azurefunctions.extensions.http.fastapi as fast_api
+import aiohttp
 
 blueprint = func.Blueprint()
 
 
 @blueprint.function_name(name="proxy")
-async def main(request: fast_api.Request) -> fast_api.Response:
+async def main(request: func.HttpRequest) -> func.HttpResponse:
     logging.info("Processing HTTP request...")
 
-    # Extract destination URL from the body
-    destination_url = request.get_json().get("url")
-    if not destination_url:
-        return func.HttpResponse("Missing destination URL.", status_code=400)
-
-    # Make the request to the destination URL
-    response = fast_api. requests.get(destination_url)
-
-    return fast_api.Response(content=response.content, status_code=response.status_code)
+    return func.HttpResponse("Processed.", status_code=200)
